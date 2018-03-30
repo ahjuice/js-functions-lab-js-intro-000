7f7947d13d9e7c4a2fe144ccbc8b362d5c34cba9
Christopher Agius <chrisagius@fastmail.com>
2018-03-30 00:01:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(holiday, days) {
-  return  `It is ${day} days until ${holiday}!`
+  return  `It's ${days} days until ${holiday}!`
 }
